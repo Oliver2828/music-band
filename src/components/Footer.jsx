73a37ad775e0b028a7cd3c06d72111518f6ab71c
Oliver2828 <oliverezenwaka95@gmail.com>
2025-04-27 13:52:30 +0100

    foot

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,8 @@ function Footer() {
   return (
     <div className='w-full bg-[#414141] text-[11px] font-serif text-white h-[5vh] grid justify-center items-center md:relative'>
       <p>© 2019 by TRIUPMH ACOUSTIC BAND.</p>
+      {/* <p>© 2019 by TRIUPMH ACOUSTIC BAND.</p> */}
+
     </div>
   )
 }
